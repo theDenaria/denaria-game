61b9208eb23cf589ab5a07ac4dd8afc5edfde12c
OnatKorucu <onatkorucu@gmail.com>
2024-08-23 16:26:19 +0300

    feat: UIZeitnot folder added.

---
+++ Assets/_Project/UIZeitnot/ButtonZeitnot/Scripts/ButtonZeitnot.cs
@@ -0,0 +1,11 @@
+using strange.extensions.signal.impl;
+using UnityEngine.UI;
+
+namespace _Project.UIZeitnot.ButtonZeitnot.Scripts
+{
+	public class ButtonZeitnot : Button
+	{
+		internal Signal ButtonClickedSignal = new Signal();
+	}
+}
+
---
+++ Assets/_Project/UIZeitnot/SliderZeitnot/SliderZeitnot.cs
@@ -0,0 +1,10 @@
+using UnityEngine.UI;
+
+namespace _Project.UIZeitnot.SliderZeitnot
+{
+    public class SliderZeitnot : Slider
+    {
+
+    }
+}
+
